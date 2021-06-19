06657aa8f9655780ca8e57269e43e100a5e63e6d
Fabiola Montoya M <75088452+fabimmu@users.noreply.github.com>
2021-06-19 08:56:10 -0600

    Generar grilla random

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grilla = document.querySelector(".grilla");
-  const widthFacil = 9;
+  const widthFacil = 8;
   const squaresFacil = [];
+
+  //const frutas = ["kiwi", "coco", "peach", "limon", "cereza", "sandia"];
+  const colores = ["green", "red", "pink", "blue", "black", "orange"];
 
   //Crear la grilla
 
   const crearTablero = () => {
     for (let i = 0; i < widthFacil * widthFacil; i++) {
       const cuadrado = document.createElement("div");
+      let randomFruta = Math.floor(Math.random() * colores.length);
+      cuadrado.style.backgroundColor = colores[randomFruta];
       grilla.appendChild(cuadrado);
       squaresFacil.push(cuadrado);
     }
