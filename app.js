a93db85b2dd12b09644178bca15972bdc3d260a5
Fabiola Montoya M <75088452+fabimmu@users.noreply.github.com>
2021-06-21 19:39:51 -0600

    Asignando img a c/celda

--- app.js
+++ app.js
@@ -4,18 +4,38 @@ document.addEventListener("DOMContentLoaded", () => {
   const squaresFacil = [];
 
   //const frutas = ["kiwi", "coco", "peach", "limon", "cereza", "sandia"];
-  const colores = ["green", "red", "pink", "blue", "black", "orange"];
 
-  //Crear la grilla
+  // const frutas = [
+  //   { name: "limon", image: "url(styles/img/1F34B_color.png)" }, //lemon
+  //   { name: "kiwi", image: "url(styles/img/1F95D_color.png)" },
+  //   { name: "sandia", image: "url(styles/img/1F349_color.png)" },
+  //   { name: "peach", image: "url(styles/img/1F351_color.png)" },
+  //   { name: "cherry", image: "url(styles/img/1F352_color.png)" },
+  //   { name: "coco", image: "url(styles/img/1F965_color.png)" },
+  // ];
 
-  const crearTablero = () => {
+  const frutas = [
+    "url(./styles/img/1F34B_color.png)",
+    "url(./styles/img/1F95D_color.png)",
+    "url(./styles/img/1F349_color.png)",
+    "url(./styles/img/1F351_color.png",
+    "url(./styles/img/1F352_color.png)",
+    "url(./styles/img/1F965_color.png)",
+  ];
+
+  function createBoard() {
     for (let i = 0; i < widthFacil * widthFacil; i++) {
-      const cuadrado = document.createElement("div");
-      let randomFruta = Math.floor(Math.random() * colores.length);
-      cuadrado.style.backgroundColor = colores[randomFruta];
-      grilla.appendChild(cuadrado);
-      squaresFacil.push(cuadrado);
+      const square = document.createElement("div");
+      //square.setAttribute("draggable", true);
+      square.setAttribute("class", "item");
+      let randomColor = Math.floor(Math.random() * frutas.length);
+      square.style.backgroundImage = frutas[randomColor];
+      document.getElementsByClassName("item")[i].style.backgroundSize = "cover";
+      console.log(document.getElementsByClassName("item"));
+      grilla.appendChild(square);
+      squaresFacil.push(square);
     }
-  };
-  crearTablero();
+  }
+
+  createBoard();
 });
