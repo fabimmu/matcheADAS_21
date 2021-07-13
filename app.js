ded694f0f6fe7b561ba4c6d1e08b57be9e9bcbc9
fabimmu <fabi.montoya20@gmail.com>
2021-07-13 17:15:36 -0600

    Implementacion de sweetalerts

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const $$ = (selector) => document.querySelectorAll(selector);
 
   const frutas = [
-    "url(./styles/img/1F34B_color.png)",
-    "url(./styles/img/1F95D_color.png)",
-    "url(./styles/img/1F349_color.png)",
-    "url(./styles/img/1F351_color.png",
-    "url(./styles/img/1F352_color.png)",
-    "url(./styles/img/1F965_color.png)",
+    "url(./styles/img/limon.png)",
+    "url(./styles/img/kiwi.png)",
+    "url(./styles/img/sandia.png)",
+    "url(./styles/img/melocoton.png",
+    "url(./styles/img/cereza.png)",
+    "url(./styles/img/coco.png)",
   ];
 
   /* Random */
@@ -42,164 +42,29 @@ document.addEventListener("DOMContentLoaded", () => {
       }
       grilla.appendChild(square);
       squares.push(square);
-
-      square.addEventListener("dragstart", dragStart);
-      square.addEventListener("dragover", dragOver);
-      square.addEventListener("dragenter", dragEnter);
-      square.addEventListener("dragleave", dragLeave);
-      square.addEventListener("drop", (e) => dragDrop(tamanio, e));
-
-      //Seleccionar item
-      square.addEventListener("click", () => {
-        square.style.opacity = "0.5";
-      });
     }
+    squares.forEach((square) =>
+      square.addEventListener("dragstart", dragStart)
+    );
+    squares.forEach((square) =>
+      square.addEventListener("dragend", (e) => dragEnd(tamanio, e))
+    );
+    squares.forEach((square) => square.addEventListener("dragover", dragOver));
+    squares.forEach((square) =>
+      square.addEventListener("dragenter", dragEnter)
+    );
+    squares.forEach((square) =>
+      square.addEventListener("dragleave", dragLeave)
+    );
+    squares.forEach((square) =>
+      square.addEventListener("drop", (e) => dragDrop(tamanio, e))
+    );
   };
 
   /* Limpiando Grilla */
   const clearBoard = () => {
     $(".grilla").innerHTML = "";
   };
-
-  /* Mover item */
-  let fruitDragged;
-  let fruitReplaced;
-  let squareIdDragged;
-  let squareIdReplaced;
-
-  const dragStart = (e) => {
-    fruitDragged = e.target.style.backgroundImage;
-    squareIdDragged = parseInt(e.target.id);
-  };
-
-  const dragOver = (e) => {
-    e.preventDefault();
-  };
-  const dragEnter = (e) => {
-    e.preventDefault();
-  };
-  const dragLeave = (e) => {
-    if (squareIdDragged == e.target.id) {
-      e.target.style.backgroundImage = "";
-    }
-  };
-  const dragDrop = (tamanio, e) => {
-    let validMoves = [
-      squareIdDragged - 1,
-      squareIdDragged - tamanio,
-      squareIdDragged + 1,
-      squareIdDragged + tamanio,
-    ];
-
-    fruitReplaced = e.target.style.backgroundImage;
-    squareIdReplaced = parseInt(e.target.id);
-
-    let validMove = validMoves.includes(squareIdReplaced);
-    if (validMove) {
-      squares[squareIdReplaced].style.backgroundImage = fruitDragged;
-      squares[squareIdDragged].style.backgroundImage = fruitReplaced;
-    }
-  };
-
-  /* Inicializar modales */
-
-  const modales = () => {
-    $("#btn-jugar").addEventListener("click", () => {
-      $("#modal-bienvenida").classList.add("oculto");
-      $("#modal-nuevo-juego").classList.remove("oculto");
-    });
-
-    $("#btn-modo-facil").addEventListener("click", () => {
-      clearBoard();
-      createBoard(9, 630);
-      dificultad = "facil";
-      $("#modal-nuevo-juego").classList.add("oculto");
-      restartTimer();
-    });
-    $("#btn-modo-normal").addEventListener("click", () => {
-      clearBoard();
-      createBoard(8, 560);
-      dificultad = "normal";
-      $("#modal-nuevo-juego").classList.add("oculto");
-      restartTimer();
-    });
-    $("#btn-modo-dificil").addEventListener("click", () => {
-      clearBoard();
-      createBoard(7, 490);
-      dificultad = "dificil";
-      $("#modal-nuevo-juego").classList.add("oculto");
-      restartTimer();
-    });
-  };
-
-  modales();
-
-  /*Button Info */
-
-  let info = false;
-  $("#info-btn").addEventListener("click", () => {
-    info = true;
-    clearInterval(timedown);
-    $("#modal-bienvenida").classList.remove("oculto");
-    if (info) {
-      $("#btn-jugar").addEventListener("click", () => {
-        $("#modal-nuevo-juego").classList.add("oculto");
-        continueTimer();
-      });
-    }
-  });
-
-  /*Refresh Button*/
-
-  $("#refresh-btn").addEventListener("click", () => {
-    //Timer must stop
-    clearInterval(timedown);
-    $("#modal-reiniciar-juego").classList.remove("oculto");
-  });
-
-  $("#btn-cancelar").addEventListener("click", () => {
-    $("#modal-reiniciar-juego").classList.add("oculto");
-    continueTimer();
-  });
-
-  $("#btn-nuevo-juego").addEventListener("click", () => {
-    $("#modal-reiniciar-juego").classList.add("oculto");
-    $("#modal-nuevo-juego").classList.remove("oculto");
-  });
-
-  /* Juego Terminado */
-
-  const gameOver = () => {
-    $("#modal-juego-terminado").classList.remove("oculto");
-
-    $("#btn-nuevo-juego2").addEventListener("click", () => {
-      $("#modal-juego-terminado").classList.add("oculto");
-      $("#modal-nuevo-juego").classList.remove("oculto");
-    });
-
-    $("#btn-reiniciar-juego").addEventListener("click", () => {
-      $("#modal-juego-terminado").classList.add("oculto");
-
-      switch (dificultad) {
-        case "facil":
-          clearBoard();
-          createBoard(9, 630);
-          restartTimer();
-          break;
-        case "normal":
-          clearBoard();
-          createBoard(8, 560);
-          restartTimer();
-          break;
-        case "dificil":
-          clearBoard();
-          createBoard(7, 490);
-          restartTimer();
-          break;
-      }
-    });
-  };
-
   /* Timer */
   /* const countdown = () => {
     timedown = setInterval(function () {
@@ -226,4 +91,124 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }, 1000);
   };
-});
+
+  ///Modales Sweet Alert
+  const textspan = document.createElement("span");
+  textspan.innerHTML =
+    "En MatcheADAs tu objetivo es juntar tres o más ítems del mismo tipo, ya sea en fila o columna. Para eso, selecciona un ítem y a continuación un ítem adyacente para intercambiarlos de lugar.<br><br> Si se forma un grupo, esos items se eliminarán y ganarás puntos. ¡Sigue armando grupos de 3 o más antes de que se acabe el tiempo! <br><br><strong>Controles</strong><br>Click izquierdo: Selección <br>Enter o Espacio: Selección <br>Flechas o WASD: Movimiento e intercambio";
+  /*Bienvenida*/
+  const swalBienvenida = () => {
+    swal({
+      title: "¡Bienvenida!",
+      content: textspan,
+      button: "A jugar",
+      closeOnClickOutside: false,
+    }).then(() => {
+      swalNiveles();
+    });
+  };
+  swalBienvenida();
+
+  /*Nuevo Juego*/
+  const prueba = () => {
+    console.log("Hola hola");
+  };
+
+  const swalNiveles = () => {
+    swal({
+      title: "Nuevo Juego",
+      text: "Seleccione una dificultad",
+      buttons: {
+        facil: {
+          text: "Facil",
+          value: "facil",
+        },
+        medio: {
+          text: "Normal",
+          value: "normal",
+        },
+        dificil: {
+          text: "Dificil",
+          value: "dificil",
+        },
+      },
+    }).then((value) => {
+      switch (value) {
+        case "facil":
+          prueba();
+          clearBoard();
+          createBoard(9, 630);
+          //restartTimer();
+          break;
+        case "normal":
+          console.log(value);
+          clearBoard();
+          createBoard(8, 560);
+          //restartTimer();
+          break;
+        case "dificil":
+          console.log(value);
+          clearBoard();
+          createBoard(7, 490);
+          //restartTimer();
+          break;
+      }
+    });
+  };
+  /*Reiniciar Juego*/
+  const swalreiniciar = () => {
+    swal({
+      title: "Reiniciar juego?",
+      text: "Perderás todo tu puntaje acumulado!",
+      closeOnClickOutside: false,
+      buttons: {
+        cancel: "Cancelar",
+        nuevoJuego: {
+          text: "Nuevo juego",
+          value: "resetear",
+        },
+      },
+    }).then((value) => {
+      if (value === "resetear") {
+        swalNiveles();
+      }
+    });
+  };
+  /*Termino Juego*/
+  const swalGameOver = () => {
+    swal({
+      title: "¡Juego terminado!",
+      text: "Puntaje final: 0",
+      closeOnClickOutside: false,
+      buttons: {
+        reiniciar: "Reiniciar",
+        nuevoJuego: {
+          text: "Nuevo juego",
+          value: "resetear",
+        },
+      },
+    }).then((value) => {
+      if (value === "resetear") {
+        swalNiveles();
+      }
+    });
+  };
+  /*Boton Info*/
+  infoBtn = document.getElementById("info-btn");
+  infoBtn.addEventListener("click", () => {
+    info = true;
+    if (info) {
+      swal({
+        title: "¡Bienvenida!",
+        content: textspan,
+        button: "A jugar",
+        closeOnClickOutside: false,
+      });
+    }
+  });
+  /*Boton Refresh*/
+  refreshBtn = document.getElementById("refresh-btn");
+  refreshBtn.addEventListener("click", () => {
+    swalreiniciar();
+  });
+}); //DOM cierre
